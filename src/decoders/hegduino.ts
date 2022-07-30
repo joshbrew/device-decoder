13834ee11108ba7bde1fec8205cd7e117d85ca3f
Bubba Skrimp <brewster.joshua1@gmail.com>
2022-07-29 18:25:20 -0700

    Update hegduino.ts

--- src/decoders/hegduino.ts
+++ src/decoders/hegduino.ts
@@ -9,20 +9,27 @@ export default function hegduinocodec(value:ArrayBuffer) {
         red: 0,
         infrared: 0,
         ratio: 0,
-        temperature: 0
+        ambient: 0,
+        temperature: 0 //temp on v2, nonsense on v1
     }
 
     let txt = textdecoder.decode(value);
     let line = txt.split(','); //serial will stream in as utf8 lines
-    if(line.length >= 5) {
+    if(line.length === 3) { //android web ble mode (20 byte packet lim)
+        output.timestamp = Date.now();
+        output.red = parseInt(line[0]);
+        output.infrared = parseInt(line[1]);
+        output.ratio = parseFloat(line[2]);
+
+    } else if(line.length >= 5) {
         output.timestamp = parseInt(line[0]);
         output.red = parseInt(line[1]);
         output.infrared = parseInt(line[2]);
         output.ratio = parseFloat(line[3]);
-        output.temperature = parseFloat(line[4]);
+        output.ambient = parseFloat(line[4]);
+        output.temperature = parseFloat(line[5]);
 
         return output;
 
-    }
-    else return txt;
+    } else return txt; //e.g. echoed commands or startup/crash messages
 }
