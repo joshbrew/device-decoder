ee8449c20053bbe2fa60898cca12de6a84273a95
Joshua Brewster (Bubba Skrimp) <brewster.joshua1@gmail.com>
2023-01-16 01:02:41 -0800

    edit dist

--- dist/devices/blueberry2.d.ts
+++
@@ -1,6 +0,0 @@
-import { WebglLinePlotProps } from "webgl-plot-utils";
-import { BLEDeviceOptions } from "../ble/ble_client";
-export declare function blueberryshortcodec(value: DataView): any;
-export declare function blueberrylongcodec(value: DataView): any;
-export declare const blueberry2BLESettings: BLEDeviceOptions;
-export declare const blueberry2ChartSettings: Partial<WebglLinePlotProps>;
--- dist/devices/bme280.d.ts
+++
@@ -1,4 +0,0 @@
-import { WebglLinePlotProps } from "webgl-plot-utils";
-export declare const sealevel_hpa = 1013.25;
-export declare const bme280codec: (data: any) => any;
-export declare const bme280ChartSettings: Partial<WebglLinePlotProps>;
